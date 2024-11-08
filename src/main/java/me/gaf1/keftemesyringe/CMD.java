acff992d382f9fc34154e6d8ab9b54549eb3eaef
Gaf1k <dimasikpr337@gmail.com>
2024-11-08 18:43:19 +0500

    Update CMD.java

--- src/main/java/me/gaf1/keftemesyringe/CMD.java
+++ src/main/java/me/gaf1/keftemesyringe/CMD.java
@@ -3,9 +3,12 @@ package me.gaf1.keftemesyringe;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 
-public class CMD implements CommandExecutor {
+import java.util.List;
+
+public class CMD implements TabExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)){
@@ -13,7 +16,24 @@ public class CMD implements CommandExecutor {
             return true;
         }
         Player player = (Player) sender;
-        Syringe.instance.getSyringe(player);
+        if (args.length == 0){
+            Syringe.instance.getSyringe(player);
+            return true;
+        }
+        if (args[0].equalsIgnoreCase("reload")) {
+            Plugin.getInstance().reloadConfig();
+            player.sendMessage(Syringe.color(Plugin.getInstance().getConfig().getString("reload_config_message")));
+            return true;
+        }
         return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if (args.length == 1){
+            return List.of("reload");
+        }
+
+        return null;
     }
 }
